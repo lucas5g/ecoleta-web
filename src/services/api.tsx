e25c19aaa9f97a80be4de042033b2cee73da6f7b
Lucas <lucasdesousa19@hotmail.com>
2020-07-02 22:19:26 -0300

    Update api.tsx

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 const hostname = () => {
   const app = window.location.hostname
 
-  return app === 'ecoleta-regiao.netlify.app' ? 'https://ecoleta-lucas.heroku.com' :  'http://localhost:8000'
+  return app === 'ecoleta-regiao.netlify.app' ? 'https://ecoleta-lucas.herokuapp.com/' :  'http://localhost:8000'
 }
 
 const api = axios.create({
